44e1d76bd5f9ff1249db1f878dc6233068e04a4e
adithirgis <adithi@ilklabs.com>
2021-03-25 15:06:07 +0530

    added 7

---
+++ 2021_03_20_Tips_Tricks_RMarkdown_Meetup/my_function.R
@@ -0,0 +1,11 @@
+# Tip 1: A function shortcut
+
+fruit <- function(apples, bananas, oranges, berries) {
+  apples + bananas + oranges*berries
+}
+
+
+
+animals <- function(sharks, eagle, banana, pika) {
+  sharks*eagle + banana - 4.2*pika
+}
